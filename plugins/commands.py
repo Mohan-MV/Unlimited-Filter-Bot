de93e30fe80d4b3ffc93abef1febbdb4d52df2da
Master <85671955+abhimohan-tech@users.noreply.github.com>
2021-09-14 12:19:11 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -243,12 +243,12 @@ async def help(client, message):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("How to Deploy?", url="https://youtu.be/hkmc3e7U7R4"),
+                    InlineKeyboardButton("How to Deploy?", url="https://t.me/AdhavaaBiriyaniKittiyalo"),
                     InlineKeyboardButton("About Me", callback_data="about_data")
                 ],
                 [
-                    InlineKeyboardButton("BOT Channel", url="https://t.me/TroJanzHEX"),
-                    InlineKeyboardButton("Support Group", url="https://t.me/TroJanzSupport")
+                    InlineKeyboardButton("Update Channel", url="https://t.me/new_moviyzz"),
+                    InlineKeyboardButton("Support Group", url="https://t.me/ff_new_movies_group")
                 ]
             ]
         ),
@@ -265,7 +265,7 @@ async def about(client, message):
             [
                 [
                     InlineKeyboardButton(
-                        "SOURCE CODE", url="https://github.com/TroJanzHEX/Unlimited-Filter-Bot")
+                        "SOURCE CODE", url="https://t.me/AdhavaaBiriyaniKittiyalo")
                 ],
                 [
                     InlineKeyboardButton("BACK", callback_data="help_data"),
